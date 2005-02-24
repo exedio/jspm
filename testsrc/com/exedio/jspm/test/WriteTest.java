8487ba8fa271dc956e44a2bc0b58b7fbf8733df8
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-02-24 11:59:19 +0000

    test an argument
    git-svn-id: https://svn.exedio.com/cope/trunk/jspm@1792 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/jspm/test/WriteTest.java
+++ testsrc/com/exedio/jspm/test/WriteTest.java
@@ -9,24 +9,48 @@ import junit.framework.TestCase;
 
 public class WriteTest extends TestCase
 {
-	public static final String EXPECTED = 
+	public static final String EXPECTED_CONDITION_PREFIX = 
 		"<html>\n" +
 		"\t<head>\n" +
 		"\t\t<title>234</title>\n" +
-		"\t</head>\n" +
+		"\t</head>";
+
+	public static final String EXPECTED_CONDITION_BODY = 
+		"\n" +
 		"\t<body>\n" +
 		"\t\thello-zapp\n" +
-		"\t</body>\n" +
+		"\t</body>";
+	
+	public static final String EXPECTED_CONDITION_POSTFIX = 
+		"\n" +
 		"</html>\n" +
 		"\n";
 
+	public static final String EXPECTED_CONDITION_TRUE = 
+		EXPECTED_CONDITION_PREFIX +
+		EXPECTED_CONDITION_BODY +
+		EXPECTED_CONDITION_POSTFIX;
+
+	public static final String EXPECTED_CONDITION_FALSE = 
+		EXPECTED_CONDITION_PREFIX +
+		EXPECTED_CONDITION_POSTFIX;
+
 	public void testWrite()
 	{
-		final StringWriter buf = new StringWriter();
-		final PrintWriter print = new PrintWriter(buf);
-		Test_Jspm.writeIt(print);
-		print.flush();
-		assertEquals(EXPECTED, buf.getBuffer().toString());
+		{
+			final StringWriter buf = new StringWriter();
+			final PrintWriter print = new PrintWriter(buf);
+			Test_Jspm.writeIt(print, true);
+			print.flush();
+			assertEquals(EXPECTED_CONDITION_TRUE, buf.getBuffer().toString());
+		}
+		{
+			final StringWriter buf = new StringWriter();
+			final PrintWriter print = new PrintWriter(buf);
+			Test_Jspm.writeIt(print, false);
+			print.flush();
+			assertEquals(EXPECTED_CONDITION_FALSE, buf.getBuffer().toString());
+		}
 	}
 
 }
