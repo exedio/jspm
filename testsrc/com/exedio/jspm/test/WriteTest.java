994231c0dc52880a16cc4e4b839fb34095956142
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-05-31 16:26:51 +0000

    execute removeTrailingWhitespace.sh
    git-svn-id: https://svn.exedio.com/cope/trunk/jspm@5725 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/jspm/test/WriteTest.java
+++ testsrc/com/exedio/jspm/test/WriteTest.java
@@ -28,29 +28,29 @@ import junit.framework.TestCase;
 
 public class WriteTest extends TestCase
 {
-	public static final String EXPECTED_CONDITION_PREFIX = 
+	public static final String EXPECTED_CONDITION_PREFIX =
 		"<html>\n" +
 		"\t<head>\n" +
 		"\t\t<title>234</title>\n" +
 		"\t</head>";
 
-	public static final String EXPECTED_CONDITION_BODY = 
+	public static final String EXPECTED_CONDITION_BODY =
 		"\n" +
 		"\t<body>\n" +
 		"\t\thello-zapp\n" +
 		"\t</body>";
 	
-	public static final String EXPECTED_CONDITION_POSTFIX = 
+	public static final String EXPECTED_CONDITION_POSTFIX =
 		"\n" +
 		"</html>\n" +
 		"\n";
 
-	public static final String EXPECTED_CONDITION_TRUE = 
+	public static final String EXPECTED_CONDITION_TRUE =
 		EXPECTED_CONDITION_PREFIX +
 		EXPECTED_CONDITION_BODY +
 		EXPECTED_CONDITION_POSTFIX;
 
-	public static final String EXPECTED_CONDITION_FALSE = 
+	public static final String EXPECTED_CONDITION_FALSE =
 		EXPECTED_CONDITION_PREFIX +
 		EXPECTED_CONDITION_POSTFIX;
 
